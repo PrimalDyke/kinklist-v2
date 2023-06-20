836075a7510074fd254469a38a9dbe1f5a9c832b
PrimalDyke <137196438+PrimalDyke@users.noreply.github.com>
2023-06-21 06:51:35 +1000

    Update idGenerator.ts

--- src/util/idGenerator.ts
+++ src/util/idGenerator.ts
@@ -1,6 +1,6 @@
 export const generateId = (): string => {
   return new Array(6)
     .fill(0)
-    .map(() => Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(36))
+    .map(() => Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(40))
     .join('-');
 };
